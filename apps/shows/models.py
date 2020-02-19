f5c8edf7ee4166a282bd7fa775026f14dffc1a3d
tommyn707 <tommy.n707@gmail.com>
2020-02-19 15:17:03 -0800

    second

--- apps/shows/models.py
+++ apps/shows/models.py
@@ -1,6 +1,27 @@
+from __future__ import unicode_literals
 from django.db import models
+from datetime import datetime, date
 
-# Create your models here.
+
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {
+
+        }
+
+        if len(postData['title']) < 2:
+            errors['title'] = "Title should be at least 2 characters long!"
+        if Show.objects.filter(title=postData['title']).exists():
+            errors['title_repeat'] = "Title should be unique!"
+        if len(postData['network']) < 3:
+            errors['network'] = "Network should be at least 3 characters long!"
+        if len(postData['desc']) < 10 and len(postData['desc']) is not 0:
+            errors['desc'] = "Description should be at least 10 characters long!"
+        return errors
+
+
+
 class Show(models.Model):
     title = models.CharField(max_length=45)
     network = models.CharField(max_length=45)
--- apps/shows/views.py
+++ apps/shows/views.py
@@ -9,37 +9,32 @@ from .models import *
 def index(request):
     context={
         "all_shows": Show.objects.all()
-    }
+    }   
     return render(request, "shows/index.html", context)
 
-# Add a show
+
 def add_show(request):
     return render(request, "shows/add_show.html" )
 
-# Submit a show to db
+
 def submit_show(request):
     print(request.POST)
     Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], description=request.POST['description'])
     return redirect("/shows")
 
-# Show details of a specific show
 def display_show(request, id):
     context = {
         "show": Show.objects.get(id=id)
     }
     return render(request, "shows/display_show.html", context)
 
-# Edit a show
 def edit_show(request, id):
-    # print(Show.objects.get(id=id).release_date)
     context = {
         "show": Show.objects.get(id=id),
         "date": str(Show.objects.get(id=id).release_date)
     }
-    # print(context["show"].release_date)
     return render(request, "shows/edit_show.html", context)
 
-# Submit an edit
 def submit_edit(request, id):
     selected = Show.objects.get(id=id)
     if request.POST['title']:
@@ -56,7 +51,6 @@ def submit_edit(request, id):
     selected.save()
     return redirect("/shows")
 
-# Delete a show
 def delete_show(request, id):
     Show.objects.get(id=id).delete()
     return redirect("/shows")
